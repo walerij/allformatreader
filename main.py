7318955e5e34bd8b800cd2ed2e6e325095419bd5
ZHdanov Valery <becwal@yandex.ru>
2025-05-26 01:28:17 +0300

    создание файлов-обработчиков в папке utils и функции main в основном коде, получение расширения файла

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import argparse, os, os.path
+from pathlib import Path
+def main():
+    parser = argparse.ArgumentParser(description="allformatreader - считывает данные всех форматов в текст через консоль")
+    parser.add_argument("--file", help="Путь к обрабатываемому файлу")
+    parser.add_argument("--path", help="Каталог, где хранятся обрабатываемые файлы ")
+    #parser.add_argument("--clean", action="store_true", help="Очистка текста")
+    args = parser.parse_args()
+    #print(args)
+    if args.file:
+        extension = Path(args.file).suffix
+        print(extension)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+if __name__ == "__main__":
+    main()
