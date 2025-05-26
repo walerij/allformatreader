9e1eb1eeb03b1a11ccce169ac4e22b8bf84d80ce
ZHdanov Valery <becwal@yandex.ru>
2025-05-26 23:38:52 +0300

    вызов классов обработки файлов .txt .docx .doc

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse, os, os.path
 from pathlib import Path
+from utils.txtreader import Txtreader
+from utils.docreader import Docreader
 def main():
     parser = argparse.ArgumentParser(description="allformatreader - считывает данные всех форматов в текст через консоль")
     parser.add_argument("--file", help="Путь к обрабатываемому файлу")
@@ -7,10 +9,22 @@ def main():
     #parser.add_argument("--clean", action="store_true", help="Очистка текста")
     args = parser.parse_args()
 
+    txt_ = Txtreader()
+    doc_ = Docreader()
+
+
     #если параметр  - файл
     if args.file:
+        result_text = ""
         extension = Path(args.file).suffix # получаем расширение файла
-        print(extension) # печатаем его
+        if extension==".txt":
+            result_text = txt_.extact_text(args.file)
+        elif extension==".docx":
+            result_text = doc_.extact_text_docx(args.file)
+        elif extension == ".doc":
+            result_text = doc_.extact_text_doc(args.file)
+
+        print(result_text)
 
     elif args.path:
         #получаем имена файлов директории
